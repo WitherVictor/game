ffc5e48a63d0a1c8799b0f04df48a4f75f8d05bc
WitherVictor <number42@yeah.net>
2026-01-26 00:15:36 +0800

    实现一个血条

---
+++ util/color.hpp
@@ -0,0 +1,7 @@
+#pragma once
+
+#include "imgui.h"
+
+namespace Color {
+    inline constexpr auto Red = ImVec4(1.00f, 0.00f, 0.00f, 1.00f);
+}
--- view/view.hpp
+++ view/view.hpp
@@ -7,18 +7,33 @@
 #include "imgui.h"
 
 // Project
-#include "model/electricity.hpp"
+#include "model/player.hpp"
+#include "util/color.hpp"
+
+namespace {
+    constexpr inline static auto default_window_config = 
+        ImGuiWindowFlags_NoMove |
+        ImGuiWindowFlags_NoCollapse |
+        ImGuiWindowFlags_AlwaysAutoResize |
+        ImGuiWindowFlags_NoScrollbar |
+        ImGuiWindowFlags_NoScrollWithMouse;
+}
 
 namespace view {
     static void status_window() {
-        ImGui::Begin("资源");
+        ImGui::SetNextWindowPos({0, 0}, ImGuiCond_Always);
+        ImGui::Begin("人物状态", nullptr, default_window_config);
 
-        ImGui::ProgressBar(model::electricity.progress());
+        ImGui::Text("%s", "Health");
+        ImGui::SameLine();
+        ImGui::PushStyleColor(ImGuiCol_PlotHistogram, Color::Red);
+        ImGui::ProgressBar(model::player.health().ratio(), ImVec2{});
+        ImGui::PopStyleColor();
 
         ImGui::End();
     }
 
     static void update_all(const std::chrono::milliseconds& elapsed_time) {
-        model::electricity.update(elapsed_time);
+        
     }
 };
