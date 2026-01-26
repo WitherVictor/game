e06564cbca2ffc6aea8deea66d4b8a127f2afe9e
WitherVictor <number42@yeah.net>
2026-01-26 14:29:04 +0800

    添加饥饿值和口渴值状态条

--- model/player.hpp
+++ model/player.hpp
@@ -12,11 +12,18 @@ public:
     const auto health() const {
         return health_.values();
     }
+
+    const auto hunger() const {
+        return hunger_.values();
+    }
+
+    const auto thirst() const {
+        return thirst_.values();
+    }
 private:
     atomic_clamped<std::size_t> health_{0, 100, 100};
-    atomic_clamped<std::size_t> hunger_{0, 100};
-    atomic_clamped<std::size_t> thirst_{0, 100};
-    atomic_clamped<std::size_t> stamina_{0, 100};
+    atomic_clamped<std::size_t> hunger_{0, 100, 50};
+    atomic_clamped<std::size_t> thirst_{0, 100, 50};
 };
 
 inline player_impl player;
--- view/window/status.cpp
+++ view/window/status.cpp
@@ -12,18 +12,29 @@ namespace view::window {
         ImGuiWindowFlags_NoScrollbar |
         ImGuiWindowFlags_NoScrollWithMouse;
 
+    template <typename T>
+    inline void make_status_bar(const char* status_name, typename clamped<T>::info current_status, ImVec4 color) {
+        // 状态名
+        ImGui::Text("%s", status_name);
+        ImGui::SameLine();
+
+        // 设置进度条颜色
+        ImGui::PushStyleColor(ImGuiCol_PlotHistogram, color);
+        ImGui::ProgressBar(current_status.ratio, ImVec2{});
+        ImGui::PopStyleColor();
+        ImGui::SameLine();
+
+        // 显示当前值与最大值
+        ImGui::Text("[%zu/%zu]", current_status.now, current_status.max);
+    }
+
     inline void status() {
         ImGui::SetNextWindowPos({0, 0}, ImGuiCond_Always);
         ImGui::Begin("人物状态", nullptr, default_window_config);
 
-        const auto player_health = model::player.health();
-        ImGui::Text("%s", "Health");
-        ImGui::SameLine();
-        ImGui::PushStyleColor(ImGuiCol_PlotHistogram, Color::Red);
-        ImGui::ProgressBar(player_health.ratio, ImVec2{});
-        ImGui::PopStyleColor();
-        ImGui::SameLine();
-        ImGui::Text("[%zu/%zu]", player_health.now, player_health.max);
+        make_status_bar<std::size_t>("生命值", model::player.health(), Color::Red);
+        make_status_bar<std::size_t>("饥饿值", model::player.hunger(), Color::Brown);
+        make_status_bar<std::size_t>("口渴值", model::player.thirst(), Color::LightBlue);
 
         ImGui::End();
     }
