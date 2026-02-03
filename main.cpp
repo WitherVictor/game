9d7b07cc9784b9ff62df54fecb8dbb4e7aa0114d
WitherVictor <number42@yeah.net>
2026-02-03 17:20:28 +0800

    将 ImGui 的初始化逻辑放到 main 函数内

--- main.cpp
+++ main.cpp
@@ -1,13 +1,122 @@
-#include "view/view.hpp"
-#include "model/model.hpp"
+#include "GLFW/glfw3.h"
 
-#include "item/food.hpp"
-#include <memory>
+#include "imgui_impl_glfw.h"
+#include "imgui_impl_opengl3.h"
+#include "view/view.hpp"
+
+#include <stdexcept>
+
+
+// 全局 glfw 错误处理回调函数
+static void glfw_error_callback(int error_code, const char* description) {
+    fprintf(stderr, "GLFW Error %d: %s\n", error_code, description);
+}
 
 int main() {
-    view view_{model::instance()};
+    constexpr const char* title = "Escape from Charlie Station";
+    constexpr std::size_t window_width = 1280;
+    constexpr std::size_t window_height = 800;
 
-    model::instance().inventory.add_item(std::make_shared<protein_bar>(3));
+    // 设置全局回调函数
+    glfwSetErrorCallback(glfw_error_callback);
 
-    view_.run();
+    // 如果 glfw 初始化失败，则直接抛出异常
+    if (!glfwInit())
+        throw std::runtime_error{"GLFW initialization failed!"};
+
+    // 初始化窗口
+    auto main_scale = ImGui_ImplGlfw_GetContentScaleForMonitor(glfwGetPrimaryMonitor());
+    auto window = glfwCreateWindow(
+        (int)(window_width * main_scale),
+        (int)(window_height * main_scale),
+        title,
+        nullptr,
+        nullptr
+    );
+    glfwMakeContextCurrent(window);
+    glfwSwapInterval(1);
+
+
+    // Decide GL+GLSL versions
+#if defined(IMGUI_IMPL_OPENGL_ES2)
+        // GL ES 2.0 + GLSL 100 (WebGL 1.0)
+        auto backend_version = "#version 100";
+        glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 2);
+        glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 0);
+        glfwWindowHint(GLFW_CLIENT_API, GLFW_OPENGL_ES_API);
+#elif defined(IMGUI_IMPL_OPENGL_ES3)
+        // GL ES 3.0 + GLSL 300 es (WebGL 2.0)
+        auto backend_version = "#version 300 es";
+        glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+        glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 0);
+        glfwWindowHint(GLFW_CLIENT_API, GLFW_OPENGL_ES_API);
+#elif defined(__APPLE__)
+        // GL 3.2 + GLSL 150
+        auto backend_version = "#version 150";
+        glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+        glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 2);
+        glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);  // 3.2+ only
+        glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);            // Required on Mac
+#else
+        // GL 3.0 + GLSL 130
+        auto backend_version = "#version 130";
+        glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+        glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 0);
+        //glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);  // 3.2+ only
+        //glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);            // 3.0+ only
+#endif
+
+    // 检查 ImGui 版本并创建上下文
+    IMGUI_CHECKVERSION();
+    ImGui::CreateContext();
+
+    // 创建 IO 并启用键盘导航
+    auto& io = ImGui::GetIO();
+    io.ConfigFlags |= ImGuiConfigFlags_NavEnableKeyboard;
+
+    // 添加中文字符集
+    io.Fonts->AddFontFromFileTTF(
+        "C:\\Windows\\Fonts\\msyh.ttc",
+        18.0f,
+        nullptr,
+        io.Fonts->GetGlyphRangesChineseFull()
+    );
+
+    // 设置 ImGui 样式
+    ImGui::StyleColorsClassic();
+
+    // 设置平台与渲染后端
+    ImGui_ImplGlfw_InitForOpenGL(window, true);
+    ImGui_ImplOpenGL3_Init(backend_version);
+
+    model model_obj;
+    view view_obj = model_obj;
+
+    // 运行窗口循环
+    while (!glfwWindowShouldClose(window)) {
+        // 轮询事件
+        glfwPollEvents();
+
+        // 开始 ImGui 帧
+        ImGui_ImplOpenGL3_NewFrame();
+        ImGui_ImplGlfw_NewFrame();
+        ImGui::NewFrame();
+
+        view_obj.draw();
+
+        ImGui::Render();
+        int display_w, display_h;
+        glfwGetFramebufferSize(window, &display_w, &display_h);
+        glViewport(0, 0, display_w, display_h);
+        glClearColor(0.45f, 0.55f, 0.60f, 1.00f);
+        glClear(GL_COLOR_BUFFER_BIT);
+        ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
+
+        glfwSwapBuffers(window);
+    }
+
+    // 清理资源
+    ImGui_ImplOpenGL3_Shutdown();
+    ImGui_ImplGlfw_Shutdown();
+    ImGui::DestroyContext();
 }
--- view/view.hpp
+++ view/view.hpp
@@ -2,13 +2,9 @@
 
 // STL
 #include <cstdio>
-#include <stdexcept>
 #include <string>
 
 // ImGui
-#include "GLFW/glfw3.h"
-#include "imgui_impl_glfw.h"
-#include "imgui_impl_opengl3.h"
 #include "imgui.h"
 
 // Project
@@ -29,17 +25,9 @@ enum class main_window_t {
     engi_powerstore,
 };
 
-// 全局 glfw 错误处理回调函数
-static void glfw_error_callback(int error_code, cstring description) {
-    fprintf(stderr, "GLFW Error %d: %s\n", error_code, description);
-}
-
 class view {
 public:
-    // 定义标题，窗口宽度与长度
-    constexpr inline static cstring title = "Escape from Charlie Station";
-    constexpr inline static std::size_t window_width = 1280;
-    constexpr inline static std::size_t window_height = 800;
+    // 默认窗口设置
     constexpr inline static ImGuiWindowFlags default_window_config{
         ImGuiWindowFlags_NoMove            |
         ImGuiWindowFlags_NoCollapse        |
@@ -47,117 +35,17 @@ public:
         ImGuiWindowFlags_AlwaysAutoResize
     };
 
-    // 定义后端版本字符串
-    inline static cstring backend_version = nullptr;
-
     view(model& model_)
-        : model_{model_}
-    {
-        window_init();
-        check_backend_version();
-        imgui_init();
-    }
+        : model_{model_} {}
 
-    ~view() {
-        // 清理资源
-        ImGui_ImplOpenGL3_Shutdown();
-        ImGui_ImplGlfw_Shutdown();
-        ImGui::DestroyContext();
-    }
-
-    //  使用 glfw 创建窗口
-    void window_init() {
-        //  设置全局回调函数
-        glfwSetErrorCallback(glfw_error_callback);
-
-        // 如果 glfw 初始化失败，则直接抛出异常
-        if (!glfwInit())
-            throw std::runtime_error{"GLFW initialization failed!"};
-
-        // 初始化窗口
-        auto main_scale = ImGui_ImplGlfw_GetContentScaleForMonitor(glfwGetPrimaryMonitor());
-        window_ = glfwCreateWindow(
-            (int)(window_width * main_scale),
-            (int)(window_height * main_scale),
-            title,
-            nullptr,
-            nullptr
-        );
-        glfwMakeContextCurrent(window_);
-        glfwSwapInterval(1);
-    }
-
-    // 判断后端使用的版本
-    void check_backend_version() {
-        // Decide GL+GLSL versions
-#if defined(IMGUI_IMPL_OPENGL_ES2)
-        // GL ES 2.0 + GLSL 100 (WebGL 1.0)
-        backend_version = "#version 100";
-        glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 2);
-        glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 0);
-        glfwWindowHint(GLFW_CLIENT_API, GLFW_OPENGL_ES_API);
-#elif defined(IMGUI_IMPL_OPENGL_ES3)
-        // GL ES 3.0 + GLSL 300 es (WebGL 2.0)
-        backend_version = "#version 300 es";
-        glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
-        glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 0);
-        glfwWindowHint(GLFW_CLIENT_API, GLFW_OPENGL_ES_API);
-#elif defined(__APPLE__)
-        // GL 3.2 + GLSL 150
-        backend_version = "#version 150";
-        glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
-        glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 2);
-        glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);  // 3.2+ only
-        glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);            // Required on Mac
-#else
-        // GL 3.0 + GLSL 130
-        backend_version = "#version 130";
-        glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
-        glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 0);
-        //glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);  // 3.2+ only
-        //glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);            // 3.0+ only
-#endif
-    }
-
-    void imgui_init() {
-        // 检查 ImGui 版本并创建上下文
-        IMGUI_CHECKVERSION();
-        ImGui::CreateContext();
-
-        // 创建 IO 并启用键盘导航
-        auto& io = ImGui::GetIO();
-        io.ConfigFlags |= ImGuiConfigFlags_NavEnableKeyboard;
-
-        // 添加中文字符集
-        io.Fonts->AddFontFromFileTTF(
-            "C:\\Windows\\Fonts\\msyh.ttc",
-            18.0f,
-            nullptr,
-            io.Fonts->GetGlyphRangesChineseFull()
-        );
-
-        // 设置 ImGui 样式
-        ImGui::StyleColorsClassic();
-
-        // 设置平台与渲染后端
-        ImGui_ImplGlfw_InitForOpenGL(window_, true);
-        ImGui_ImplOpenGL3_Init(backend_version);
-    }
-
-    void run() {
-        while (!glfwWindowShouldClose(window_)) {
-            run_loop_start();
-
-            ImGui::SetNextWindowPos(ImVec2{});
-            const auto status_window = view::player_status();
-            ImGui::SetNextWindowPos({
-                status_window->Pos.x,
-                status_window->Size.y
-            });
-            view::side_menu();
-
-            run_loop_end();
-        }
+    void draw() {
+        ImGui::SetNextWindowPos(ImVec2{});
+        const auto status_window = view::player_status();
+        ImGui::SetNextWindowPos({
+            status_window->Pos.x,
+            status_window->Size.y
+        });
+        view::side_menu();
     }
 
     // 绘制人物状态栏
@@ -365,30 +253,6 @@ public:
         ImGui::End();
         return window;
     }
-
-    void run_loop_start() {
-        // 轮询事件
-        glfwPollEvents();
-
-        // 开始 ImGui 帧
-        ImGui_ImplOpenGL3_NewFrame();
-        ImGui_ImplGlfw_NewFrame();
-        ImGui::NewFrame();
-    }
-
-    void run_loop_end() {
-        ImGui::Render();
-        int display_w, display_h;
-        glfwGetFramebufferSize(window_, &display_w, &display_h);
-        glViewport(0, 0, display_w, display_h);
-        glClearColor(0.45f, 0.55f, 0.60f, 1.00f);
-        glClear(GL_COLOR_BUFFER_BIT);
-        ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
-
-        glfwSwapBuffers(window_);
-    }
 private:
-    GLFWwindow* window_;
-
     model& model_;
 };
