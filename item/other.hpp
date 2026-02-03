4aabfbb782b519609b919785d278c37adde36237
WitherVictor <number42@yeah.net>
2026-02-03 10:32:45 +0800

    新建其他物品类型，添加冰块

---
+++ item/other.hpp
@@ -0,0 +1,24 @@
+#ifndef ITEM_OTHER_HPP
+#define ITEM_OTHER_HPP
+
+#include "item.hpp"
+#include "model/model.hpp"
+
+class ice_block : public usable_item {
+    ice_block(std::size_t amount)
+        : usable_item{
+            "冰块",
+            amount,
+            "一颗内部含有一些杂质的冰块，"
+            "为了安全着想，最好融化煮沸后饮用。",
+            [] {
+                auto& player = model::instance().player;
+
+                player.health.force_minus(5);
+                player.hunger.force_minus(10);
+                player.thirst.force_add(100);
+            }
+        } {}
+};
+
+#endif // End of macro ITEM_OTHER_HPP
